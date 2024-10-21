a943e1f6c81803ce16a74f74632c67497cb61d64
Victor Elgersma <victor.elgersma41@thg.com>
2024-10-21 10:45:16 +0100

    array stuff

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ## Todo
 
 - [x] fix no zeros for heros
+
+## Resources
+
+
+the python tutorial: https://docs.python.org/3/library/functions.html#max
--- sum.py
+++ sum.py
@@ -1,10 +1,24 @@
+from collections.abc import Iterable
+
+'''
+https://www.codewars.com/kata/576b93db1129fcf2200001e6/train/python
+- filtering out None type
+'''
+
 def sum_array(arr):
     '''
     >>> sum_array([1,2,3])
     2
-    >>> sum_array[4,5,6,7]
+    >>> sum_array([4,5,6,7])
     11
+    >>> sum_array(None)
+    0
     '''
+    if isinstance(arr,Iterable) and len(arr) > 1:
+
+        filtered = [x for x in arr if x is not None]
+        return sum(filtered) - max(filtered) - min(filtered)
+    else: return 0
 
 
 if __name__ == "__main__":
