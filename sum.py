f4748068efb49d5081997d8f18d77fc49bb0810d
Victor Elgersma <146109489+victorelgersma@users.noreply.github.com>
2024-10-21 09:08:20 +0000

    started new python kata

---
+++ sum.py
@@ -0,0 +1,12 @@
+def sum_array(arr):
+    '''
+    >>> sum_array([1,2,3])
+    2
+    >>> sum_array[4,5,6,7]
+    11
+    '''
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
