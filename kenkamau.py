315555fc774d8b7f63fba7041d1301a43d80bd35
Victor Elgersma <victor.elgersma41@thg.com>
2024-10-24 14:36:21 +0100

    improved

--- kenkamau.py
+++ kenkamau.py
@@ -1,20 +1,20 @@
-def capitalize(s):
+def capitalize_list(s):
     """
-    >>> capitalize("abcdef")
+    >>> capitalize_list("abcdef")
     ['AbCdEf', 'aBcDeF']
+    >>> capitalize_list("")
+    ['', '']
     """
-    first = ''
-    second = ''
-    for i in range(len(s)):
+    first = []
+    second = []
+    for i, char in enumerate(s):
         if i % 2 == 0:
-            first += s[i].upper()
-            second += s[i]
+            first.append(char.upper())
+            second.append(char)
         else:
-            first += s[i]
-            second += s[i].upper()
-    return [first, second]
-
-
+            first.append(char)
+            second.append(char.upper())
+    return [''.join(first), ''.join(second)]
 
 
 if __name__ == "__main__":
