df7ea49c8f6b0f3bf31fd2c09c32498dbfa75261
Victor Elgersma <victor.elgersma41@thg.com>
2024-11-05 23:24:25 +0000

    feat: currency converter

--- 8-kyu/usd-cny.py
+++ 8-kyu/usd-cny.py
@@ -3,8 +3,11 @@ def usdcny(usd):
     """
     >>> usdcny(15)
     '101.25 Chinese Yuan'
+    >>> usdcny(2244)
+    '15147.00 Chinese Yuan'
     """
-    pass
+    cny_amount = float(usd) * 6.75
+    return f"{cny_amount:.2f} Chinese Yuan"
 
 if __name__ == "__main__":
     import doctest
