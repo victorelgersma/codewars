455afa9120f4838279e44f204b405c90276a4c0a
Victor Elgersma <victor.elgersma41@thg.com>
2025-01-27 10:16:31 +0000

    completed

---
+++ 25-01/alphanumeric.py
@@ -0,0 +1,19 @@
+def alphabet_position(text):
+    """
+    >>> alphabet_position("The sunset sets at twelve o' clock.")
+    '20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11'
+    """
+    output_list= []
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    for char in text.lower():
+        if char.isalpha():
+            output_list.append(str(alphabet.find(char)+1))
+    output = " ".join(output_list)
+    
+    return output
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
+
