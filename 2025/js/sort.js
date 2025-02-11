8b04090cdea273c1fac0bd03c0b6502f63e53f7f
Victor Elgersma <victor.elgersma41@thg.com>
2025-02-11 07:01:39 +0000

    sorted

---
+++ 2025/js/sort.js
@@ -0,0 +1,13 @@
+
+// sorts solution
+
+function solution(nums){
+    if (nums === null) {
+        return []
+    }
+    return nums.sort((a,b) => a-b)
+}
+
+
+console.log(solution([1, 2, 10, 50, 5])); // should return [1,2,5,10,50]
+console.log(solution(null)); // should return []
